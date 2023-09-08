7c4ffa9e8df7c42fa832a43e074975ed0a858e23
Gustavo de Souza <gustavodesouza111.gs@gmail.com>
2023-09-08 18:57:56 -0300

    questao 3

--- Listas de exercícios/Lista02/q2.c
+++ Listas de exercícios/Lista02/q2.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 
-int main(){
+int maior(int &vetor[3]){
 
-    printf("Insira 3 numeros inteiros:\n");
-    for(int i = 0; i < 3; i++){
-        scanf("%f", &vetor[i]);
-    }
-
-    //ordenando em ordem decrescente o vetor dos lados:
-    float aux;
+       //ordenando em ordem decrescente o vetor dos lados:
+    int aux;
     for(int i = 0; i<3; i++){
         for(int j = 0; j < 3; j++){
             if(vetor[i] > vetor[j]){
@@ -20,8 +15,21 @@ int main(){
     }
 
     //salvando as posicoes do vetor em variaveis fixas
-    x = vetor[0];
-    y = vetor[1];
-    z = vetor[2];
+    int x = vetor[0];
+    
+    return x;
+
+}
+
+int main(){
+
+    int vetor[3];
+
+    printf("Insira 3 numeros inteiros:\n");
+    for(int i = 0; i < 3; i++){
+        scanf("%i", &vetor[i]);
+    }
+
+    maior(&vetor[3]);
 
 }
---
+++ Listas de exercícios/Lista02/q3.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+
+
+int analise(char c){
+
+    int x;
+
+    switch( c ){
+        case '0':
+        x = 0;
+        break;
+
+        case '1':
+        x = 1;
+        break;
+
+
+        case '2':
+        x = 2;
+        break;
+
+
+        case '3':
+        x = 3;
+        break;
+
+
+        case '4':
+        x = 4;
+        break;
+
+
+        case '5':
+        x = 5;
+        break;
+
+
+        case '6':
+        x = 6;
+        break;
+
+
+        case '7':
+        x = 7;
+        break;
+
+
+        case '8':
+        x = 8;
+        break;
+
+
+        case '9':
+        x = 9;
+        break;
+
+        default:
+        printf("O caracter inserido nao é um digito\n");
+    } 
+
+    printf("%i", x);
+
+}
+
+
+int main(){
+
+    char c;
+
+    printf("Insira um caracter:\n");
+    scanf("%c", &c);
+    
+    analise(c);
+
+}
